7b56b9a98d02f2d1a65690667ea37b702db49cc6
Sawan Kumar <103842962+sawanjr@users.noreply.github.com>
2023-11-02 13:13:17 +0530

    Update langchain_intigration.py

--- langchain_intigration.py
+++ langchain_intigration.py
@@ -1,25 +1,17 @@
-from langchain.llms import OpenAI  # Import OpenAI from the right module
-from langchain import PromptTemplate, LLMChain
+from langchain import PromptTemplate, OpenAI, LLMChain
 from dotenv import load_dotenv
-load_dotenv()
-# from langchain.agents import load_tools, initialize_agent, AgentType
 
-def generate_emails(subject, tone):
+load_dotenv()
+
+def generate_email(subject, tone):
     llm = OpenAI(temperature=0.5)
-    prompt_template_name = PromptTemplate(
-        input_variables=["subject", "tone"],
-        template=f"generate a brief e-mail on the subject {subject} and make sure the tone is {tone} in 1 line only"
-    )
+    prompt_template_name = PromptTemplate(input_variables=["subject", "tone"], template="generate a brief e-mail on the subject {subject} and make sure the tone is {tone} in 20 line only")
 
     name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="text")
 
-    response = name_chain({"subject": subject, "tone": tone})  # Correct the key
+    response = name_chain({"subject": subject, "tone": tone})
     return response
 
-# def langchain_agent():
-#     llm = OpenAI(temperature=0.5)
-#     tools = load_tools(["Grammarly", "Hemingway Editor", "ProWritingAid"], llm=llm)
-#     agent = initialize_agent(llm, tools, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True)
-
 if __name__ == "__main__":
-    print(generate_emails("email to the boss for not attending the meeting", "crying"))
+    result = generate_email("email to the boss for not attending the meeting", "american english")
+    print(result)
