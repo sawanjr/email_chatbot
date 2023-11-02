3b25aa96c3b92da59cf5c860f8b695696ad05bf1
Sawan Kumar <103842962+sawanjr@users.noreply.github.com>
2023-11-02 12:07:42 +0530

    Update langchain_intigration.py

--- langchain_intigration.py
+++ langchain_intigration.py
@@ -1,22 +1,25 @@
-from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.llms import OpenAI  # Import OpenAI from the right module
+from langchain import PromptTemplate, LLMChain
 from dotenv import load_dotenv
 load_dotenv()
-from langchain.agents import load_tools , initialize_agent , AgentType
+# from langchain.agents import load_tools, initialize_agent, AgentType
 
-def generate_emails(subject , tone ):
-    llm= OpenAI(temperature = 0.5)
-    prompt_template_name = PromptTemplate(input_variables=["subject" , "tone" ],template = f"generate a brief e-mail on the subject{subject} and make sure the tone is {tone} in 1 line only")
+def generate_emails(subject, tone):
+    llm = OpenAI(temperature=0.5)
+    prompt_template_name = PromptTemplate(
+        input_variables=["subject", "tone"],
+        template=f"generate a brief e-mail on the subject {subject} and make sure the tone is {tone} in 1 line only"
+    )
 
-    name_chain = LLMChain(llm = llm , prompt = prompt_template_name , output_key ="text")
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="text")
 
-    response = name_chain({"object":object , "tone":tone})
+    response = name_chain({"subject": subject, "tone": tone})  # Correct the key
     return response
+
 # def langchain_agent():
-#     llm = OpenAI(temperature = 0.5)
-#     tools = load_tools(["Grammarly", "Hemingway Editor" , "ProWritingAid"], llm = llm)
-#     agent  = initialize_agent(llm , tools,agent = AgentType.CONVERSATIONAL_REACT_DESCRIPTION , verbose = true)
+#     llm = OpenAI(temperature=0.5)
+#     tools = load_tools(["Grammarly", "Hemingway Editor", "ProWritingAid"], llm=llm)
+#     agent = initialize_agent(llm, tools, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True)
+
 if __name__ == "__main__":
-    print(generate_emails("email to the boss for not attending the meeting","crying"))
-
-
-
+    print(generate_emails("email to the boss for not attending the meeting", "crying"))
